68d3a4f0fbd299f0ce403f7fb4978ef6c3728a68
Idisimagha Dublin-Green <greendublin007@gmail.com>
2025-06-11 01:53:07 +0100

    cleaning constants

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,11 +1,6 @@
-// const URL = 'http://localhost:8081/graphql';
-// const AUTH_URL = 'http://localhost:9085/graphql';
-// const AUTH_URL = 'http://localhost:5000/graphql';
 const URL = 'https://api.wanzami.tv/graphql';
 const AUTH_URL = 'https://auth.wanzami.tv/graphql';
 const APP = 'WANZAMI'
 const YEAR = '2025';
-console.log(process.env.APP);
 const COUNTRY_NAME_API = 'https://ipapi.co/json/';
-// const PAYSTACK_KEY = "pk_test_472a1f94c9759dfbe5088d1d891ea2a6d402179e";
-const PAYSTACK_KEY = "pk_live_b0efaf3abd8ec69c437375d16c7afeccecf59346";
+const PAYSTACK_KEY = 'pk_live_b0efaf3abd8ec69c437375d16c7afeccecf59346';
