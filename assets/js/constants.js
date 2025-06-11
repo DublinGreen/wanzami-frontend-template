c332d30eba803239be29fb85af9571913aa73bea
Idisimagha Dublin-Green <greendublin007@gmail.com>
2025-06-11 01:25:10 +0100

    trying amplify environment variables

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -5,6 +5,7 @@ const URL = 'https://api.wanzami.tv/graphql';
 const AUTH_URL = 'https://auth.wanzami.tv/graphql';
 const APP = 'WANZAMI'
 const YEAR = '2025';
+console.log(process.env.APP);
 const COUNTRY_NAME_API = 'https://ipapi.co/json/';
 // const PAYSTACK_KEY = "pk_test_472a1f94c9759dfbe5088d1d891ea2a6d402179e";
 const PAYSTACK_KEY = "pk_live_b0efaf3abd8ec69c437375d16c7afeccecf59346";
